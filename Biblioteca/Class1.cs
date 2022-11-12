e0439098e0c7192dc34e1c0b25368c91fb08d373
Amin Boudlale <AMIN.BOUDLALE@local.ittcesena.it>
2022-11-12 09:12:46 +0100

    Versione 2: creazione classe biblioteca con costruttore

---
+++ Biblioteca/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    internal class Class1
+    {
+        string nome, indirizzo;
+        int orarioApertura, orarioChiusura;
+        public Class1(string nome, string indirizzo, int orarioApertura, int orarioChiusura)
+        {
+            this.nome = nome;
+            this.indirizzo = indirizzo;
+            this.orarioApertura = orarioApertura;
+            this.orarioChiusura = orarioChiusura;
+        }
+        
+    }
+}
